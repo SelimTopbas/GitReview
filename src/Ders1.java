71d26ddc1f8d0d2fcf138cc3913a77762b51b3fd
SelimTopbas <selimtopbas@gmail.com>
2020-10-20 21:25:03 +0200

    branchten once master

--- src/Ders1.java
+++ src/Ders1.java
@@ -8,6 +8,7 @@ public class Ders1 {
 		System.out.println("Saat 19:17 - Iskender");
 
 		System.out.println("Saat 19:28 - Hamza");
+		System.out.println("Saat 18:01 - feature branch 1. satir");
 
 		
 		
